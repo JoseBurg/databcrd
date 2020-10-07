d4c5cd1119a0e02a83121e164417042606f34759
JoseBurg <joseaburgos01@outlook.com>
2020-10-07 03:09:07 +0000

    n1

--- Sector_Externo/Datos economicos.R
+++ Sector_Externo/Datos economicos.R
@@ -12,8 +12,16 @@ read_excel(desm1)
 url2<-"https://cdn.bancentral.gov.do/documents/estadisticas/sector-externo/documents/bpagos_6.xls?v=1602038237421"
 desm2<-file.path("~","local_bpagos_6.xls?v=1602038237421")
 download.file(url2,desm2)
-read_excel(desm2)
+blpanual<-read_excel(desm2)
 
+### Limpiando la base de datos 
+
+blpanual<-read_excel(desm2,skip = 8,col_names = c("Conceptos","2010","2011",
+                                                  "2012","2013","2014","2015",
+                                                  "2016","2017","2018","2019"))
+blpanual<-blpanual[-c(64:70),]
+blpanual<-blpanual[-c(10,19,30,38,40,42,50,52,54),]
+View(blpanual)
 ### Balanza de pagos trimestral
 
 url2.1<-"https://cdn.bancentral.gov.do/documents/estadisticas/sector-externo/documents/bpagos__trim_6.xls?v=1602038237421"
